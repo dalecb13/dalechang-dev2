4b8f55f29e6f0c10df0f6731e2a6f3ed8f1fa645
dalechangdev <dalecb13@gmail.com>
2026-07-09 17:04:41 +0200

    Used climate hero

--- src/app/next/page.tsx
+++ src/app/next/page.tsx
@@ -1,8 +1,10 @@
+import ClimateHero from "./components/climate-hero";
 
 export default function Next() {
     return (
         <>
             <h1>TBD</h1>
+            <ClimateHero />
         </>
     )
 }
